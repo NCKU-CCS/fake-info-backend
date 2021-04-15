5d91e37ebd047694de193d4b0cb55a6e5e52d6c2
williron3960 <kjyang@netdb.csie.ncku.edu.tw>
2021-04-15 17:30:29 +0800

    feat: update environment variables

--- sql_app/pg_db.py
+++ sql_app/pg_db.py
@@ -1,13 +1,26 @@
 import databases, sqlalchemy
 import datetime, random, uuid
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String ,DateTime ,Boolean
+from databases import DatabaseURL
+from starlette.config import Config
+from starlette.datastructures import Secret
 
-#connect to database
-DATABASE_URL = 'postgresql://netdb:netdb2602@db:5432/news_postgres'
+# config
+
+config = Config("./../.env")
+
+POSTGRES_USER = config("POSTGRES_USER", cast=str)
+POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", cast=Secret)
+POSTGRES_SERVER = config("POSTGRES_SERVER", cast=str, default="db")
+POSTGRES_PORT = config("POSTGRES_PORT", cast=str, default="5432")
+POSTGRES_DB = config("POSTGRES_DB", cast=str)
+
+# connect to database
+DATABASE_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}'
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
 
-#define table
+# define table
 users = sqlalchemy.Table(
     'result',
     metadata,
